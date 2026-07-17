9098f9cbfadadf428f770486b1335efb9baf7d85
Jati Sri Pamungkas <jatispamungkas357@gmail.com>
2026-07-17 08:40:36 +0700

    feat: add global error

--- src/server.js
+++ src/server.js
@@ -25,6 +25,17 @@ app.get('/', (req, res) => {
   res.send('Welcome to Gard Backend!');
 });
 
+// Global Error Handler
+app.use((err, req, res, next) => {
+  console.error('[Error Handler]', err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    status: statusCode === 500 ? 'error' : 'fail',
+    message: err.message || 'Internal Server Error',
+    ...(process.env.NODE_ENV !== 'production' && { stack: err.stack }),
+  });
+});
+
 // Listening to the port
 app.listen(port, hostName, () => {
   console.log(`[Server] running in ${env} mode on http://${hostName}:${port}`);
